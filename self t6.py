0249d62af343052b65e0a9e9f1efc210cbdbd85e
anujsah762-stack <anujsah762@gmail.com>
2026-08-29 12:25:17 +0545

    Add files via upload

---
+++ self t6.py
@@ -0,0 +1,6 @@
+import turtle as t
+for i in range(85):
+    t.circle(50)
+    t.right(5)
+    t.speed(0)
+t.done()
---
+++ self t8.py
@@ -0,0 +1,9 @@
+import turtle as t
+for i in range(85):
+    t.right(5)
+    for j in range(4):
+        t.shape("turtle")
+        t.forward(100)
+        t.right(90)
+        t.speed(0)
+t.done()
